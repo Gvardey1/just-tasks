7fcc28a46fd1436d8bd2bb807d03faf9fb06c4fc
Hordii <hordeyigarfild@gmail.com>
2025-11-18 23:59:15 +0100

    fourth

--- test1.js
+++ test1.js
@@ -5,3 +5,20 @@ function sumArray(array) {
 }
 
 console.log("Sum is:", sumArray(arr));
+
+
+
+let arr2 = [5, 4, 77, 21, 62];
+
+function maxInArray(array) {
+    return Math.max(...array);
+}
+
+console.log("Max number in array is:", maxInArray(arr2));
+
+
+let arr3 = [10, 20, 30, 40, 50];
+function averageArray(array) {
+    const sum = array.reduce((acc, val) => acc + val, 0);
+    return sum / array.length;
+}
